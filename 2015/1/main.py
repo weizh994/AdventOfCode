3e1b216ebe7e47e3ac0d6efe040c25f4177b9261
Weixiong Zheng <weizh994@proton.me>
2024-12-09 15:41:27 +0100

    add py code for 2015 day1

---
+++ 2015/1/main.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    file = open("input", "r")
+    floor = 0
+    for c in file.read():
+        if c == "(":
+            floor += 1
+        elif c == ")":
+            floor -= 1
+    print(f"Question 1:{floor}")
+    floor = 0
+    for i, c in enumerate(open("input", "r").read()):
+        floor += 1 if c == "(" else -1
+        if floor == -1:
+            print(f"Question 2:{i + 1}")
+            break
